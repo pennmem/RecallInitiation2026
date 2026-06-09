c07ff9b507c073e00d655e78ed504c55c43eb61c
stlzdev <andrewwong2005@gmail.com>
2026-06-08 22:31:33 -0600

    update scripts for modified exp 2

--- fetch_dirfr.sh
+++ fetch_dirfr.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
-# Fetch latest dirFR data from cmlpsiturk to local data_storage
+# Fetch latest experiment data from cmlpsiturk to local data_storage.
+# Usage: ./fetch_dirfr.sh [table]
+#   ./fetch_dirfr.sh          -> fetches the new dirFRU data (table "dirfru")
+#   ./fetch_dirfr.sh dirfr    -> fetches the old data (table "dirfr")
 
 set -e  # stop on any error
 
+TABLE="${1:-dirfru}"
 LOCAL_DIR=~/RecallInitiation2026/data/data_storage
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
-FILENAME="dirfr_trials_${TIMESTAMP}.csv"
+FILENAME="${TABLE}_trials_${TIMESTAMP}.csv"
 
 mkdir -p "$LOCAL_DIR"
 
-echo "Running fetch on cmlpsiturk..."
-ssh cmlpsiturk "source ~/miniconda3/etc/profile.d/conda.sh && conda 
-activate prolific && python ~/fetch_dirfr.py"
+echo "Running fetch on cmlpsiturk for table '$TABLE'..."
+ssh cmlpsiturk "python3 ~/fetch_table.py $TABLE"
 
 echo "Copying CSV to local..."
-scp cmlpsiturk:/home/maint/dirfr_trials.csv "${LOCAL_DIR}/${FILENAME}"
+scp "cmlpsiturk:/home/maint/${TABLE}_trials.csv" "${LOCAL_DIR}/${FILENAME}"
 
 echo "Done. Saved to ${LOCAL_DIR}/${FILENAME}"
